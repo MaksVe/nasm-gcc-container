f1a7d94be81c4d0ab347a8687d68fba9dcc6003a
MaksVe <marcospoon@gmail.com>
2022-02-19 18:37:06 +0300

    Remove output files before compiling

--- nasm-gcc/src/compile.sh
+++ nasm-gcc/src/compile.sh
@@ -6,6 +6,9 @@ echo "file to compile: $file"
 filename="${file%%.*}"
 echo "bare filename: $filename"
 
+rm -f $filename.o
+rm -f $filename.out
+
 nasm -felf64 $file -o $filename.o
 ld -o $filename.out $filename.o
 chmod u+x $filename.out
